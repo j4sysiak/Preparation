69615aabc47b422970398ef7dbe5470b99f68561
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-03 20:49:45 +0200

    03-06-2022 20:49

---
+++ SOLID/SingleResponsibility/src/com/company/Invoice.java
@@ -0,0 +1,49 @@
+package com.company;
+
+import java.util.Collection;
+
+public class Invoice {
+
+    public Collection<LineItem> lineItems;
+    public String Vendor;
+    public String Vendee;
+    public String Total;
+
+    public Invoice(Collection<LineItem> lineItems, String vendor, String vendee) {
+        this.lineItems = lineItems;
+        Vendor = vendor;
+        Vendee = vendee;
+    }
+
+    public Collection<LineItem> getLineItems() {
+        return lineItems;
+    }
+
+    public String getVendor() {
+        return Vendor;
+    }
+
+    public String getVendee() {
+        return Vendee;
+    }
+
+    public String getTotal() {
+        return Total;
+    }
+
+    public void setLineItems(Collection<LineItem> lineItems) {
+        this.lineItems = lineItems;
+    }
+
+    public void setVendor(String vendor) {
+        Vendor = vendor;
+    }
+
+    public void setVendee(String vendee) {
+        Vendee = vendee;
+    }
+
+    public void setTotal(String total) {
+        Total = total;
+    }
+}
