4a34d9d0fd7876e4be8caf59d709806adabd367a
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-04 13:59:08 +0200

    04-06-2022 13:57

---
+++ SOLID/InterfaceSegregation/src/com/company/IPrinter.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public interface IPrinter {
+    void printGray(String context);
+    void printColor(String context);
+    void scan(String context);
+    void fax(String context);
+}
