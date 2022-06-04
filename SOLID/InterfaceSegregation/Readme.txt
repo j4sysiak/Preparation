2c2405d34e13f3985b10526b3cc8010124ba4ada
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-04 13:57:45 +0200

    04-06-2022 13:57

---
+++ SOLID/InterfaceSegregation/Readme.txt
@@ -0,0 +1,1 @@
+https://www.youtube.com/watch?v=prb_s7HAnP4   35:25
--- SOLID/InterfaceSegregation/src/com/company/Main.java
+++ SOLID/InterfaceSegregation/src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //Należy budować małe interfejsy. Lepiej kilka małych interfejsów niż jeden duży.
+        // Żadnego klienta nie można zmuszać do polegania na metodach, których nie używa.
+        // Podzielone interfejsy - nazywane interfejsami ról.
+        // Używamy interfejsów (jako warstwa abstrakcji) do wykonywania konkretnych zadań - mamy dzięki temu przejrzysty kod podzielony na konkretne zadania.
+
+
     }
 }
