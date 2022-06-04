0352dcd455bcfa6a5bab5d5d17ab6f5a5bc434c2
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-04 14:25:24 +0200

    04-06-2022 14:25

--- SOLID/DependencyInversion/src/com/company/Main.java
+++ SOLID/DependencyInversion/src/com/company/Main.java
@@ -3,6 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+/*
+        Wszystkie zależności z wyskopoziomowych modułów ustalających politykę dla niskopoziomowych (nadrzędnych) modułów - są odwrócone.
+        To właśnie dlatego tajniki implementacyjne wysokopoziomowych modułów są niezależne od sposobów implementacji dla niskopoziomowych modułów.
+        Moduły wysokiego poziomu nie powinny zależeć od modułów niskiego poziomu.
+        Oba powinny zależeć od abstrakcji np. interfejsów.
+        Abstrakcje nie powinny zależeć od szczegółów.
+        Szczegóły też powinny zależeć od abstrakcji.
+        Zasada Odwócenia Zależności - żeby klasy nie wiedziały o swoich tajnikach implementacyjnych
+        ale nie chodzi o hermetyzację
+        Obiekty powinny zależeć od abstrakcji a nie od typów.
+*/
+
+
+
     }
 }
