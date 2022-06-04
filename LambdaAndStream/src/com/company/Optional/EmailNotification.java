0e6032ce51043ab7992f4c498a0523d9432dff32
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-04 18:03:29 +0200

    04-06-2022 18:01

---
+++ LambdaAndStream/src/com/company/Optional/EmailNotification.java
@@ -0,0 +1,17 @@
+package com.company.Optional;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+public class EmailNotification {
+
+    public void sendNotification(User user) {
+        System.out.println("Sending notification to user: " + user.name);
+    }
+}
