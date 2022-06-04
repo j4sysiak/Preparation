74613620d787abc38fb928f541e381d8d2feb5db
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-04 12:09:06 +0200

    04-06-2022 08:04

--- SOLID/OpenCloseOnModulLevel/src/main/java/com/company/modulelevel/Application.java
+++ SOLID/OpenCloseOnModulLevel/src/main/java/com/company/modulelevel/Application.java
@@ -1,23 +1,35 @@
 package com.company.modulelevel;
 
-import java.util.List;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 public class Application {
 
-    public void render(List<Shape> shapes) {
-        for (Shape shape : shapes) {
-            ShapeType type = shape.shapeType;
-            switch (type) {
-                case Circle:
-                    renderCircle((Circle)shape);
-                    break;
-                case Rectangle:
-                    renderRectangle((Rectangle)shape);
-                    break;
-                default: return;
-            }
-        }
+    public void render(Collection<Shape> shapes) {
+//        for (Shape shape : shapes) {
+//            ShapeType type = shape.shapeType;
+//            switch (type) {
+//                case Circle:
+//                    renderCircle((Circle)shape);
+//                    break;
+//                case Rectangle:
+//                    renderRectangle((Rectangle)shape);
+//                    break;
+//                default: return;
+//            }
+//        }
+
+     shapes.stream()
+                .map(f -> {
+                    switch (f.getShapeType()) {
+                        case Circle: renderCircle((Circle)f);break;
+                        case Rectangle: renderRectangle((Rectangle)f);break;
+                    }
+                    return null;
+                })
+             .collect(Collectors.toList());
     }
+
 
     private void renderRectangle(Rectangle rectangle) {
         System.out.println("Render Rectangle ...");
