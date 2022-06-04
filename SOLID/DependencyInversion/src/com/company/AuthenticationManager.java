b07c08c41f64c17348f3ed6519120fac35ba33df
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-04 18:32:39 +0200

    04-06-2022 18:15

--- SOLID/DependencyInversion/src/com/company/AuthenticationManager.java
+++ SOLID/DependencyInversion/src/com/company/AuthenticationManager.java
@@ -12,9 +12,22 @@ import java.util.Optional;
 /*Moduł wysokopoziomowy*/
 // będzie musiała zależeć od jakiejś akstrakcji a nie bezpośrednio od klasy EmailNotification
 // więc wstawiamy nowe pole iNotificationSender Interfejsu INotificationSender
+
+// Skorzystamy tutaj z DependencyInjection:  czyli wstrzyknięcie zależności (przez konstruktor, metodę lub set())
+// DependencyInjection: to mechanizm, który pozwoli nam dostarczyć konkretną klasę z modułu niskopoziomowego (EmailNotification, SMSNotification)
+// dzięki Interfejsowi INotificationSender do klasy wysokopoziomowego modułu (tej klasy)
+//Przez konstruktor dostarczymy obiekt INotificationSender i następnie przypiszemy go do pola prywatnego iNotificationSender
+//przez to klasa wysokopoziomowego modułu nie jest już bezpośrednio zależna od modułów niskopoziomowych: (EmailNotification, SMSNotification)
+//przez to możemy w dowolny sposób dostarczać implementację jakiejś dowolnej klasy z niskopoziomowego modułu a przy tym
+// klasa wysokiego poziomu AuthenticationManager nie będzie w żaden sposób naruszona a jednocześnie będziemy mogli rozszerzyć jej funkcjonalność
 public class AuthenticationManager {
 
     private INotificationSender iNotificationSender;
+
+    //DependencyInjection
+    public AuthenticationManager(INotificationSender notificationSender) {
+        this.iNotificationSender = notificationSender;
+    }
 
     public void authenticate(User user, String email, String password) {
         Optional<String> maybeString = Optional.ofNullable(User.create("test@wp.pl").getPassword());
