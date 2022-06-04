164c437a8dc0c73c423f58fd6e9ebda86c1f1ba0
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-04 12:50:19 +0200

    04-06-2022 12:50

--- SOLID/LiskovSubstitution/src/com/company/Client.java
+++ SOLID/LiskovSubstitution/src/com/company/Client.java
@@ -4,5 +4,10 @@ public class Client {
 
     public static void main(String[] args) {
 
+        // klasa dziedzicząca powinna tylko rozszerzać możliwości klasy bazowej
+        // i nie zmieniać tego, co klasa bazowa robiła już wcześniej.
+        // Jeżeli więc tworzymy potomka, to niezależnie co znajdzie się w referencji takiego obiektu,
+        // to wywołanie metody, którą pierwotnie zdefiniowano klasie bazowej, powinno dać odpowiednie rezultaty
+
     }
 }
