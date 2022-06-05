506a17f6da9df5e3c6e668d0a18bfc1c70a074e4
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-05 12:25:21 +0200

    05-06-2022 12:08

--- Polimorfizm/src/com/company/Client.java
+++ Polimorfizm/src/com/company/Client.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import com.company.model.BankAccount;
 import com.company.model.CompanyBankAccount;
 import com.company.model.PersonalBankAccount;
 
@@ -19,12 +20,12 @@ public class Client {
         np. dla wywołania metody makeWithdrawal z typu PersonalBankAccount będzie się inaczej zachowywała, niż dla wywołania z CompanyBankAccount
         */
 
-        //BankAccount bankAccount = BankAccount.create(BigDecimal.valueOf(0));
+//        var bankAccount = BankAccount.create(BigDecimal.valueOf(0));
 
-         PersonalBankAccount bankAccount = new PersonalBankAccount(BigDecimal.valueOf(1) /*, "llll"*/);
+         var bankAccount = new PersonalBankAccount(BigDecimal.valueOf(1) /*, "llll"*/);
          bankAccount.requestPersolanLoan();
 
-//        CompanyBankAccount bankAccount = new CompanyBankAccount(BigDecimal.valueOf(1999) /*, "llll"*/);
+//        var bankAccount = new CompanyBankAccount(BigDecimal.valueOf(1999) /*, "llll"*/);
 //        bankAccount.takeLoan(BigDecimal.valueOf(300000));
 
         bankAccount.setBalance(BigDecimal.valueOf(1000));
