570127d97df72a81cff3bc92fe79e52531fc36f5
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-05 20:22:14 +0200

    05-06-2022 09:34

---
+++ WzorceProjektowe/Strategia/src/com/company/Coordinate.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import lombok.*;
+
+@Getter
+@Setter
+public class Coordinate {
+    public double longtitude;
+    public double latitude;
+}
---
+++ WzorceProjektowe/Strategia/src/com/company/IRouteStrategy.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public interface IRouteStrategy {
+
+    void CreateRoute(Coordinate start, Coordinate stop);
+}
