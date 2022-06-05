3d26ebda88b3edc12955be2c312749a4a0d22ee7
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-05 06:10:12 +0200

    05-06-2022 06:00

---
+++ DependencyInjection/src/com/company/EmailNotification.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+/*Moduł niskopoziomowy*/
+public class EmailNotification implements INotificationSender {
+
+    public void sendNotification(User user) {
+        System.out.println("Sending notification to user: " + user.name);
+    }
+}
--- DependencyInjection/src/com/company/INotificationSender.java
+++ DependencyInjection/src/com/company/INotificationSender.java
@@ -1,7 +1,5 @@
 package com.company;
 
-import com.company.model.User;
-
 public interface INotificationSender {
 
     void sendNotification(User user);
