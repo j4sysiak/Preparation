02650e018a74b00819163fde6a47d7d1d3791e28
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-05 20:21:53 +0200

    05-06-2022 09:34

--- WzorceProjektowe/strategia/src/com/company/WalkStrategy.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class WalkStrategy implements IRouteStrategy {
-    @Override
-    public void CreateRoute(Coordinate start, Coordinate stop) {
-        System.out.println("WalkStrategy ...");
-    }
-}
