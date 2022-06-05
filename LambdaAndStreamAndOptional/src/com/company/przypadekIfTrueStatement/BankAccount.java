9f93d9bc64143588af17405041df653088190fcc
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-05 09:46:46 +0200

    05-06-2022 09:16

---
+++ LambdaAndStreamAndOptional/src/com/company/przypadekIfTrueStatement/BankAccount.java
@@ -0,0 +1,14 @@
+package com.company.przypadekIfTrueStatement;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BankAccount {
+    private BigDecimal bankAccount = BigDecimal.valueOf(1000);
+}
---
+++ LambdaAndStreamAndOptional/src/com/company/przypadekIfTrueStatement/StreamExample.java
@@ -0,0 +1,48 @@
+package com.company.przypadekIfTrueStatement;
+
+import com.company.przypadekSwitchInStream.modulelevel2.Circle;
+import com.company.przypadekSwitchInStream.modulelevel2.Point;
+import com.company.przypadekSwitchInStream.modulelevel2.Rectangle;
+import com.company.przypadekSwitchInStream.modulelevel2.ShapeType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+public class StreamExample {
+
+    @BeforeEach
+    public void setUp() {
+    }
+
+    @Test
+    public void testIfTrueStatement() {
+        BigDecimal amount = new BankAccount().getBankAccount();
+        BigDecimal c =  Optional.ofNullable(amount)  // 1. sprawdzam, czy null
+                .map(a -> runIfAmountNotNull(a))            //2. jeżeli nie null
+                .orElseGet(() -> runIfEmpty());      //3. jeżeli null
+
+        System.out.println(c);
+    }
+
+
+    private BigDecimal runIfEmpty() {
+        return BigDecimal.ZERO;
+    }
+
+    private BigDecimal runIfAmountNotNull(BigDecimal amount) {
+        return Optional.of(amount.compareTo(BigDecimal.ZERO) >= 0)// 4. sprawdzam, czy true
+                .filter(Boolean::booleanValue)  // 4. będę sprawdzał Booleana
+                .map(bool -> ifTrue(amount))  // 5. Jeżeli true
+                .orElseGet(() -> ifNotTrue());   // 6. jeżeli false
+    }
+
+    private BigDecimal ifNotTrue() {
+        return BigDecimal.ZERO;
+    }
+
+    private BigDecimal ifTrue(BigDecimal amount) {
+        return amount;
+    }
+}
