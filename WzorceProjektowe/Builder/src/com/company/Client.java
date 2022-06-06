47cbd1b51d29e6cea2f541cd5a71b814a882c00b
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-06 05:58:08 +0200

    06-06-2022 05:36

--- WzorceProjektowe/Builder/src/com/company/Client.java
+++ WzorceProjektowe/Builder/src/com/company/Client.java
@@ -22,6 +22,16 @@ public class Client {
         Wszystkie metody set, w pewien sposób modyfikują fakturę, którą chcemy otrzymać.
         Końcowym momentem do wygenerowania tej faktury jest metoda build().
         Build() zwróci konkretną fakturę, którą klient chce uzyskać.
+
+        Możemy też wyabstrachować interfejsy IInvoiceBuilderVAT, IInvoiceBuilderNoneVAT, które będą implementować nasz builder.
+        Interfejsy będą zawierały deklarację wszystkich metod set() + build() do implementaccji faktury danego typu.
+        Np. faktura VAT, None_VAT, etc.
+
+        Potem wprowadzamy koncept Dyrektora (klasa Director), który będzie zarządzał tworzeniem knkretnych faktur.
+        Zawierałby on metody np. createMonthlyInvoice(), createBlankInvoice(), etc. Metody te zwracałyby obiekt Invoice.
+
+        Nasza klasa Client może używać bezpośrednio klasy tworzącej fakturę InvoiceBuilder lub z klasy Director i posłużyć się
+        konkretną metodą do tworzenia konkretnego typu faktury.
         */
     }
 }
