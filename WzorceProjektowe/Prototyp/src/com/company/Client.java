06ef0239246da9fc7fb8bb5dc6f9df57da23931d
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-06 19:51:32 +0200

    06-06-2022 07:16

--- WzorceProjektowe/Prototyp/src/com/company/Client.java
+++ WzorceProjektowe/Prototyp/src/com/company/Client.java
@@ -2,6 +2,12 @@ package com.company;
 
 public class Client {
 
+    /*
+      Prototyp:
+      kreacyjny wzorzec projektowy, który umożliwia kopiowanie istniejących już obiektów
+      bez tworzenia zależności pomiędzy kodem programu, a klasami obiektów.
+    */
+
     public static void main(String[] args) {
     }
 }
