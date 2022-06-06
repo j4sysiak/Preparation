0c607aa07420ffb080942a190a34ffe80ac3a82d
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-06 05:36:16 +0200

    06-06-2022 05:36

--- WzorceProjektowe/Builder/src/com/company/Client.java
+++ WzorceProjektowe/Builder/src/com/company/Client.java
@@ -4,5 +4,13 @@ public class Client {
 
     public static void main(String[] args) {
 
+        /*
+        Można tworzyć etapami złożone obiekty.
+        Rozdziela sposób tworzenia obiektów od ich reprezentacji.
+        Przykła z budowaniem złożonego obiektu "faktura".
+        Nie tworzymy takiego obiektu jednym konstruktorem, tylko budujemy poszczególne elementy i dokładamy do
+        obiektu "faktura".
+
+        */
     }
 }
