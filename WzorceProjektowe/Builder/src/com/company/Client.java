caf7979622ea03046a7e6ccde58186688185a0cf
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-06 05:44:12 +0200

    06-06-2022 05:36

--- WzorceProjektowe/Builder/src/com/company/Client.java
+++ WzorceProjektowe/Builder/src/com/company/Client.java
@@ -7,10 +7,21 @@ public class Client {
         /*
         Można tworzyć etapami złożone obiekty.
         Rozdziela sposób tworzenia obiektów od ich reprezentacji.
-        Przykła z budowaniem złożonego obiektu "faktura".
+        Przykła z budowaniem złożonego obiektu "faktura" (klasa Invoice).
         Nie tworzymy takiego obiektu jednym konstruktorem, tylko budujemy poszczególne elementy i dokładamy do
-        obiektu "faktura".
-
+        obiektu "faktura" (klasa Invoice).
+        Do obiektu (klasy Invoice) "faktura" wprowadzamy klasę InvoiceBuilder, której celem będzie utworzenie konkretnej faktury
+        poprzez udostępnienie takich metod jak:
+        - setVendor()
+        - setVendee()
+        - setNotes()
+        - setLineItems()
+        - setPaymentTepe()
+        - build()
+        Każda z tych metod, przyjmuje konkretne elementy (obiekty), które ustawią te parametry na fakturze.
+        Wszystkie metody set, w pewien sposób modyfikują fakturę, którą chcemy otrzymać.
+        Końcowym momentem do wygenerowania tej faktury jest metoda build().
+        Build() zwróci konkretną fakturę, którą klient chce uzyskać.
         */
     }
 }
