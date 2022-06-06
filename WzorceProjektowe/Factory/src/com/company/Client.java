8d5b1626e361656956cfe376060e445c022a8a50
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-06 07:01:13 +0200

    06-06-2022 06:35

--- WzorceProjektowe/Factory/src/com/company/Client.java
+++ WzorceProjektowe/Factory/src/com/company/Client.java
@@ -4,14 +4,13 @@ public class Client {
 
     public static void main(String[] args) throws Exception {
 
-        /*
-Udostępniany jest interface Shape do tworzenia obiektów w ramach klasy bazowej.
-Jednocześnie pozwala podklasom zmieniać typ tworzonych obiektów.
+    /*
+    Udostępniany jest interface Shape do tworzenia obiektów w ramach klasy bazowej.
+    Jednocześnie pozwala podklasom zmieniać typ tworzonych obiektów.
 
-Fabryka oddziela kod implementacyjny (tworzący dany obiekt) od kodu, który korzysta z tego obiektu.
-Dodanie nowego kształtu nie powoduje zmiany kodu klienta - dodajemy tylko w fabryce nowy case i budujemy klasę nowego obiektu
-
-        */
+    Fabryka oddziela kod implementacyjny (tworzący dany obiekt) od kodu, który korzysta z tego obiektu.
+    Dodanie nowego kształtu nie powoduje zmiany kodu klienta - dodajemy tylko w fabryce nowy case i budujemy klasę nowego obiektu.
+    */
 
         var shapeFactory = new ShapeFactory();
 
