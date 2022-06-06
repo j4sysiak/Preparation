6422f9ce22a5c373f06b956dc61f457818cc5998
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-06 07:13:30 +0200

    06-06-2022 06:35

--- WzorceProjektowe/Factory/src/com/company/Client.java
+++ WzorceProjektowe/Factory/src/com/company/Client.java
@@ -10,8 +10,8 @@ public class Client {
     Udostępniany jest interface IShape (lub lasa abstrakcyjna Shape) do tworzenia obiektów w ramach klasy bazowej.
     Jednocześnie pozwala podklasom zmieniać typ tworzonych obiektów.
 
-    Fabryka oddziela kod implementacyjny (tworzący dany obiekt) od kodu, który korzysta z tego obiektu.
-    Dodanie nowego kształtu nie powoduje zmiany kodu klienta - dodajemy tylko w fabryce nowy case i budujemy klasę nowego obiektu.
+    Fabryka oddziela kod implementacyjny (tworzący dany obiekt) od kodu, który korzysta z tego obiektu - Clienta.
+    Dodanie nowego kształtu nie powoduje zmiany kodu klienta (klasa Client) - dodajemy tylko w fabryce nowy case i budujemy klasę nowego obiektu.
     */
 
         var shapeFactory = new ShapeFactory();
