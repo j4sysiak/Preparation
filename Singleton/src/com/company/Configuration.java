d895a0540588197fee0233f2a30eaafd8270bb6f
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-07 21:12:14 +0200

    07-06-2022 21:04

--- Singleton/src/com/company/Configuration.java
+++ Singleton/src/com/company/Configuration.java
@@ -1,21 +1,30 @@
 package com.company;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Configuration {
 
     private static Configuration instance = null;
+    static ReentrantLock lock = new ReentrantLock();
 
     public String StringProperty;
     public String IntProperty;
 
     // musi być modyfikator dostępu: private
-    //tylko metoda getInstance będzie miała możliwość tworzenia obiektu - tylko jednego
+    // tylko metoda getInstance będzie miała możliwość tworzenia obiektu - tylko jednego
     private Configuration(){}
 
     // Zabezpieczenie, żeby był tylko jeden obiekt typu Configuration
     public static Configuration getInstatnce() {
-        if (instance == null) {
-            instance = new Configuration();
+
+        lock.lock ();
+        try {
+            if (instance == null) {
+                instance = new Configuration();
+            }
+            return instance;
+        } finally {
+            lock.unlock();
         }
-        return instance;
     }
 }
