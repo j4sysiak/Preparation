20e0113eaf009381843137c6ce16870675c85a98
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-07 21:23:47 +0200

    07-06-2022 20:26

--- WzorceProjektowe/Strategia/src/com/company/Client.java
+++ WzorceProjektowe/Strategia/src/com/company/Client.java
@@ -7,9 +7,11 @@ public class Client {
         var start = new Coordinate();
         var stop = new Coordinate();
 
-        //strategia pozwala na używanie różnych wariantów jednego algorytmu
-        //w obrębie danego obiektu w trakcie działania programu
-        //oddziela szczegóły implementacyjne od warstwy biznesowej
+        // Obrazowo Google map - trzy warianty: samochód, pieszo, rower
+
+        // strategia pozwala na używanie różnych wariantów jednego algorytmu
+        // w obrębie danego obiektu w trakcie działania programu
+        // oddziela szczegóły implementacyjne od warstwy biznesowej
 
         var strategy1= new BikeStrategy();
         var mapCalculator = new MapCreator(strategy1);
