bcd6618a7b01223ad55e5e15743490add34fd3b7
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-08 06:41:00 +0200

    07-06-2022 20:26

--- WzorceProjektowe/Strategia/src/com/company/Client.java
+++ WzorceProjektowe/Strategia/src/com/company/Client.java
@@ -10,7 +10,10 @@ public class Client {
         var stop = new Coordinate();
 
         // Obrazowo Google map - trzy strategie wyboru drogi do jednego celu: samochód, pieszo, rower
-
+        // Zaproponowano wzorzec strategii w kontekście klasy MapCreator
+        // W zależlości od wyboru użytkownika, klasa ta dostanie konkretną implementację dla samochód, pieszo, rower
+        // Dla klasy MapStrategy nie ma znaczenia jaką strategię dostanie
+        // strategia zostaje wstrzyknięta poprzez konstruktor do MapStrategy
         // strategia pozwala na używanie różnych wariantów jednego algorytmu
         // w obrębie danego obiektu w trakcie działania programu
         // oddziela szczegóły implementacyjne od warstwy biznesowej
