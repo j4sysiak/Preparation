2f734cc9b8fe8e1d06cd20ec3292ebf7ab10cf2f
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-11 09:47:07 +0200

    11-06-2022 09:46

--- Adapter/src/com/company/Client.java
+++ Adapter/src/com/company/Client.java
@@ -2,10 +2,15 @@ package com.company;
 
 import org.junit.jupiter.api.Test;
 
+/*
+Strukturalny wzorzec projektowy, którego celem jest umożliwienie współpracy dwóm klasom o niekompatybilnych interfejsach.
+Adapter przekształca interface jednej klasy na interfejs drugiej klasy.
+*/
+
 public class Client {
 
     @Test
     public void test() {
-	 
+
     }
 }
