1beed2b8e39d27e4fa56f6f6e6e568311f40fbc6
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-11 12:53:42 +0200

    11-06-2022 12:44

--- WzorceProjektowe/Fasada/src/com/company/DependencyScanner.java
+++ WzorceProjektowe/Fasada/src/com/company/DependencyScanner.java
@@ -1,16 +1,15 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
 public class DependencyScanner {
 
     public Collection<String> dependencyScanner(String url) {
-        System.out.println("dependencyScanner ...");
-        // return Arrays.asList("DecErr1", "DecErr2");
-        // return new ArrayList(Arrays.asList("DecErr1", "DecErr2"));
+
+        System.out.println("DependencyScanner ...");
         return List.of("DecErr1", "DecErr2");
+        // lub return Arrays.asList("DecErr1", "DecErr2");
+        // lub return new ArrayList(Arrays.asList("DecErr1", "DecErr2"));
     }
 }
--- WzorceProjektowe/Fasada/src/com/company/ScanFasade.java
+++ WzorceProjektowe/Fasada/src/com/company/ScanFasade.java
@@ -1,6 +1,11 @@
 package com.company;
 
+// zadanie fasady: agregacja czterech elementów i rozdzielenie zadań, które one (te obiekty) wykonują.
+// fasada ukrywa imlementacyjne skanowania i generowania raportu w metodzie scan
+// klient, korzystający z tej fasady, po prostu przekaże tylko urla i już.
 public class ScanFasade {
+
+    // tworzymy obiekty wszystkich typów skanowania i raportowania
     private QualityScanner qualityScanner = new QualityScanner();
     private SecurityScanner securityScanner = new SecurityScanner();
     private DependencyScanner dependencyScanner = new DependencyScanner();
@@ -13,6 +18,7 @@ public class ScanFasade {
         var securityScannerError = securityScanner.securityScanner(url);
         var dependencyScannerError = dependencyScanner.dependencyScanner(url);
 
+        //mając skany, jesteśmy w stanie wygenerować raport
         System.out.println("Scan report");
         reportGenerator.generateReport(qualityScannerError, securityScannerError, dependencyScannerError);
     }
