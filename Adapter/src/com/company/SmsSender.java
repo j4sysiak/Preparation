d4db3546908c8e6defa1baad19412befa3a08024
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-11 10:10:38 +0200

    11-06-2022 09:46

--- Adapter/src/com/company/SmsSender.java
+++ Adapter/src/com/company/SmsSender.java
@@ -1,8 +1,8 @@
 package com.company;
 
-public class SmsSender implements INotificationSender{
-    @Override
-    public void sendNotification(int userId, Notification notification) {
-        System.out.println("Sending Push notification to: " + userId + ", Title: " + notification.Title + " Body: " + notification.Body) ;
+// zewnętrzna klasa do której będziemy budować adapter
+public class SmsSender {
+    public void senSms(String to, String text) {
+        System.out.println("Sending SMS to: " + to);
     }
 }
