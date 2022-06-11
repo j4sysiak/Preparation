99efe62b88c19f2632f762b03d2c9858bb6a92fd
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-11 13:00:19 +0200

    11-06-2022 12:44

--- WzorceProjektowe/Fasada/src/com/company/Client.java
+++ WzorceProjektowe/Fasada/src/com/company/Client.java
@@ -20,6 +20,12 @@ public class Client {
     Fasada udostępni jedynie interface, przez który klient będzie mógł wykonywać różne zadania.
     Jeżeli zajdzie konieczność zmiana implementacji jakiegokolwiek typu skanowania, to korzystając z fasady
     nie będzie konieczności zmiany implementacji klienta (klasa Client)
+
+    Fasada agreguje złożone komponenty złożonego systemu i jesteśmy w stanie ukryć szczegóły implementacyjne przed klientem,
+    Klient tylko uruchamia scan fasady
+    Korzystamy z publicznego interfejsu fasady (czyli metody public void scan())
+    czyli korzystamy z łatwgo interfejsu do obsługi złożonego systemu
+    Taki system może się rozrastać w nieskończoność, a klient o tym nie wie.
 */
 
         //tworzymy fasadę
