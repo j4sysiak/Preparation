bbda277e9e0a6ce15065ed5bbb413778369c8e28
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-11 07:26:16 +0200

    11-06-2022 07:26

--- State/src/com/company/Client.java
+++ State/src/com/company/Client.java
@@ -1,5 +1,11 @@
 package com.company;
 
+/*
+Behawioralny wzorzec projektowy pozwalający na zmianę zachowania obiektu poprzez zmianę jego wewnętrznego stanu.
+Uzależnia sposób działania obiektu od stanu w jakim się aktualnie znajduje.
+
+*/
+
 public class Client {
 
     public static void main(String[] args) {
