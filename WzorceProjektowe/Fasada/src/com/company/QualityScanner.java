ba8c32f0ec4df3353de2d79c600d670ba9b31a0e
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-11 12:45:01 +0200

    11-06-2022 12:44

--- WzorceProjektowe/Fasada/src/com/company/QualityScanner.java
+++ WzorceProjektowe/Fasada/src/com/company/QualityScanner.java
@@ -1,6 +1,5 @@
 package com.company;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 
@@ -9,7 +8,7 @@ public class QualityScanner {
     public Collection<String> qualityScanner(String url) {
         System.out.println("qualityScan ...");
         return Arrays.asList("Err1", "Err2");
-        // return new ArrayList(Arrays.asList("Err1", "Err2"));
-        // return List.of("Err1", "Err2");
+        // lub return new ArrayList(Arrays.asList("Err1", "Err2"));
+        // lub return List.of("Err1", "Err2");
     }
 }
--- WzorceProjektowe/Fasada/src/com/company/SecurityScanner.java
+++ WzorceProjektowe/Fasada/src/com/company/SecurityScanner.java
@@ -7,9 +7,9 @@ import java.util.Collection;
 public class SecurityScanner {
 
     public Collection<String> securityScanner(String url) {
-        System.out.println("securityScanner ...");
-        // return Arrays.asList("SecErr1", "SecErr2");
+        System.out.println("SecurityScanner ...");
         return new ArrayList(Arrays.asList("SecErr1", "SecErr2"));
-        // return List.of("SecErr1", "SecErr2");
+        // lub return Arrays.asList("SecErr1", "SecErr2");
+        // lub eturn List.of("SecErr1", "SecErr2");
     }
 }
