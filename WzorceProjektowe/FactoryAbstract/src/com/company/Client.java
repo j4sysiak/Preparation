2d63a9ec6b8531376f4a5721fcdc1e401f3c9122
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-12 20:03:26 +0200

    12-06-2022 20:03

--- WzorceProjektowe/FactoryAbstract/src/com/company/Client.java
+++ WzorceProjektowe/FactoryAbstract/src/com/company/Client.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 public class Client {
     @Test
     public void test() {
-        
+
     /*
     Celem jest dostarczenie interfejsu do tworzenia różnych obiektów jednego typu
     dla tej samej rodziny klas bez specyfikowania ich konkretnych klas - ale nie chcemy, żeby ściśle był powiązany z jakimś konkretnym typem.
