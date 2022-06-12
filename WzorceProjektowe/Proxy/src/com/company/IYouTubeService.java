92ec4e90cbe8515a5739c25a1046f500332187b8
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-12 19:02:26 +0200

    12-06-2022 08:43

---
+++ WzorceProjektowe/Proxy/src/com/company/IYouTubeService.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public interface IYouTubeService {
+
+    byte[] getVideo(int viedeoId);
+}
---
+++ WzorceProjektowe/Proxy/src/com/company/YouTubeService.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class YouTubeService implements IYouTubeService {
+    @Override
+    public byte[] getVideo(int viedeoId) {
+
+        System.out.println("YouTubeService downloading video: " + viedeoId);
+        return new byte[1009808];
+    }
+}
