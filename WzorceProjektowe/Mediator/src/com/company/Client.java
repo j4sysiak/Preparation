350626f36eeb75da854b71a3843381cd237015cf
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-12 19:33:06 +0200

    12-06-2022 08:43

--- WzorceProjektowe/Mediator/src/com/company/Client.java
+++ WzorceProjektowe/Mediator/src/com/company/Client.java
@@ -4,6 +4,7 @@ public class Client {
 
     public static void main(String[] args) {
 
+
 //        Mediator - behawioralny wzorzec projektowy, pozwalający zredukować liczbę zależności pomiędzy różnymi obiektami.
 //        Ogranicza bezpośrednią komunikację pomiędzy obiektami input-button-checkbox i wymusza komunikację miedzy nimi za pomocą obiektu zwanego mediatorem.
 //        Dobrym przykłedem może być wieża kontroli lotów
