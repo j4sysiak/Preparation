f67817014568198502c0f84899e4c001cfd1e5c0
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-12 19:11:36 +0200

    12-06-2022 19:11

---
+++ WzorceProjektowe/ChainOfResponsibility/src/com/company/BaseHandler.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public abstract class BaseHandler implements IHandler {
+
+    protected IHandler next;
+
+    protected BaseHandler(IHandler next) {
+        this.next = next;
+    }
+
+    @Override
+    public abstract void handle(RequestContext requestContext);
+}
---
+++ WzorceProjektowe/ChainOfResponsibility/src/com/company/RequestContext.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class RequestContext {
+    public Request request;
+    public Response response;
+}
