a0a0febcf2c650485a32e15ba2184cc5f167dca1
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-12 19:53:31 +0200

    12-06-2022 19:19

--- WzorceProjektowe/Adapter/src/com/company/Client.java
+++ WzorceProjektowe/Adapter/src/com/company/Client.java
@@ -8,7 +8,6 @@ Adapter przekształca interface jednej klasy na interfejs drugiej klasy.
 Czyli zadaniem adaptera jest "Opakowanie" jednego interfejsu w nowy.
 */
 
-
 public class Client {
 
     @Test
