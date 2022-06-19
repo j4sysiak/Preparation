3c1527a008d998b0cd78be3507a51045c133c1d5
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-19 13:13:30 +0200

    19-06-2022 12:31

--- WzorceProjektowe/Decorator2/src/com/company/Main.java
+++ WzorceProjektowe/Decorator2/src/com/company/Main.java
@@ -5,19 +5,18 @@ public class Main {
     public static void main(String[] args) {
 
         //wybieramy bazowy model
-        var volvo40 = new Volvo40();
+        var v1 = new Volvo40();
         //  i dorzucamy/dekorujemy ABS'em (dodajemy ABS)
-        var absCarDecorator = new AbsCarDecorator(volvo40);
+        var absCarDecorator = new AbsCarDecorator(v1);
 
         System.out.println("Cena volvo40 typu (a ABS): " + absCarDecorator.price());
 
         //  i dorzucamy/dekorujemy radio
-        var p2 = new RadioCarDecorator(absCarDecorator);
-        System.out.println("Cena volvo40 typu (ABS + Radio): " + p2.price());
+        var v2 = new RadioCarDecorator(absCarDecorator);
+        System.out.println("Cena volvo40 typu (ABS + Radio): " + v2.price());
 
         //  i dorzucamy/dekorujemy klime
-        var p3 = new ClimaCarDecorator(p2);
-        System.out.println("Cena volvo40 typu (ABS + Radio + Klima): " + p3.price());
-
+        var v3 = new ClimaCarDecorator(v2);
+        System.out.println("Cena volvo40 typu (ABS + Radio + Klima): " + v3.price());
     }
 }
