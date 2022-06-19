60178e0bbdf8077b5c5d6c9c2cc9dedab839e5d0
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-19 13:17:15 +0200

    19-06-2022 12:31

---
+++ WzorceProjektowe/Decorator2/src/com/company/Client.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+public class Client {
+
+    @Test
+    public void test() {
+
+        //wybieramy bazowy model
+        var v1 = new Volvo40();
+        //  i dorzucamy/dekorujemy ABS'em (dodajemy ABS)
+        var absCarDecorator = new AbsCarDecorator(v1);
+
+        System.out.println("Cena volvo40 typu (a ABS): " + absCarDecorator.description() + ' ' + absCarDecorator.price());
+
+        //  i dorzucamy/dekorujemy radio
+        var v2 = new RadioCarDecorator(absCarDecorator);
+        System.out.println("Cena volvo40 typu (ABS + Radio): " + absCarDecorator.description() + ' ' + v2.price());
+
+        //  i dorzucamy/dekorujemy klime
+        var v3 = new ClimaCarDecorator(v2);
+        System.out.println("Cena volvo40 typu (ABS + Radio + Klima): " + absCarDecorator.description() + ' ' + v3.price());
+    }
+}
