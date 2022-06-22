a7bfd40d1abd14ad027d6a60ee7f2611e3a9e454
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-22 07:29:18 +0200

    22-06-2022 07:09

--- WzorceProjektowe/Proxy2/src/com/company/Proxy.java
+++ WzorceProjektowe/Proxy2/src/com/company/Proxy.java
@@ -2,14 +2,23 @@ package com.company;
 
 public class Proxy implements IService {
 
-    IService iService;
+    IService iService;  // nie tworzymy obiektu, tylko go deklarujemy
+                        // dopiero w trakcie wywołania metody method() - tworzymy ten obiekt
 
     @Override
     public void method() {
+        //druga przyczyna stosowania Proxy  np. AOP
+        //możemy też dodać jakiś fragment kodu, który wykona się przed i po odpaleniem właściwej metody na obiekcie ServiceImpl
+        //wzbogacamy kod o dodatkowe metody
+        System.out.println("Before"); //tutaj możemy wykonać jakąś kluczową czynność:  np. AOP (Aspect Object Programming)
+
+        //pierwsza przyczyna dodawania Proxy
         if (iService == null) {
             iService = new ServiceImpl();
         }
         System.out.println("Hello World from Proxy!!!!! - odwołuje się do włąściwego obiektu w klasie ServiceImpl");
         iService.method();
+
+        System.out.println("After");
     }
 }
