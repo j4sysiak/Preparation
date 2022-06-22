113a9cf754f1e16a0da57832c1bcff659ce08b43
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-22 06:19:09 +0200

    22-06-2022 06:03

--- DependencyInjectionInSpring/src/main/java/com/example/demo/CarApiController.java
+++ DependencyInjectionInSpring/src/main/java/com/example/demo/CarApiController.java
@@ -1,12 +1,24 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CarApiController {
 
-    @GetMapping
+    //Spring w momencie uruchomienia przeszukuje klasy o odpowiednich adnotacjach
+    //w klasie Car nie widzi żadnej adnotacji
+    //w klasie CarApiController widzi @RestController - więc ta klasa trafia do Spring Contextu jako Bean
+    //w klasie CarService widzi adnotację @Service - więc ta klasa trafia do Spring Contextu jako Bean
+    //w klasie CarApiController widzi adnotację na polu prywatnym @Autowired - szuka w Context Springa klasę, która jest przez niego zarządzana
+    // i widzi ją tzn. CarSerwice - bierze ją i wstrzykuje do klasy CarApiController
+
+    @Autowired
+    private CarService carService;
+
+
+    @GetMapping("/hello")
     public String seyHello() {
         return "Hello";
     }
--- DependencyInjectionInSpring/src/main/java/com/example/demo/CarService.java
+++ DependencyInjectionInSpring/src/main/java/com/example/demo/CarService.java
@@ -1,19 +1,24 @@
 package com.example.demo;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
-//obiekt takiej klasy to Singleton - tylko jedna instancja może być w obrębie całego procesu
+//obiekt takiej klasy to Singleton - tylko jedna instancja może być w obrębie całego oprogramowania
+//instancja tworzona tylko raz
 //to jest Beat Springa zarządzany przez Context Springa
 @Service
-public class CarManager {
+@Getter
+@Setter
+public class CarService {
 
     private List<Car> carList;
 
 
-    public CarManager() {
+    public CarService() {
         this.carList = new ArrayList();
         carList.add(new Car("Polo", "VW"));
         carList.add(new Car("Starlet", "Toyota"));
