376dbeeb1ad207266cbbc7301a33fe39faa1f738
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-22 07:18:50 +0200

    22-06-2022 07:09

--- WzorceProjektowe/Proxy2/src/com/company/Client.java
+++ WzorceProjektowe/Proxy2/src/com/company/Client.java
@@ -5,11 +5,13 @@ import org.junit.jupiter.api.Test;
 public class Client {
 
     //Proxy - klasa pośrednik/pełnomocnik w wywołaniu właściwej metody
+    // nasz Client nie ma bezpośrednio dostępu do metod klasy ServiceImpl
+    // ale za pośrednictwem klasy Proxy odpalamy metody w klasie ServiceImpl
 
     @Test
     public void test() {
 
-        IService iService = new ServiceImpl();
+        IService iService = new Proxy();
         iService.method();
     }
 }
--- WzorceProjektowe/Proxy2/src/com/company/Proxy.java
+++ WzorceProjektowe/Proxy2/src/com/company/Proxy.java
@@ -2,8 +2,11 @@ package com.company;
 
 public class Proxy implements IService {
 
+    IService iService = new ServiceImpl();
+
     @Override
     public void method() {
-
+        System.out.println("Hello World from Proxy!!!!! - odwołuje się do włąściwego obiektu w klasie ServiceImpl");
+        iService.method();
     }
 }
