230e1fdf1a35b93ad78e429a39a8a98f09ef22da
Jacek Sysiak <j4sysiak@gmail.com>
2023-01-15 15:46:58 +0100

    15-01-2023 15:45

---
+++ WzorceProjektowe/Prototyp/src/com/company/Client.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import org.junit.Test;
+
+public class Client {
+
+
+    /*
+      Prototyp:
+      kreacyjny wzorzec projektowy, który umożliwia kopiowanie istniejących już obiektów  (shallow and deep cloning)
+      (deep copy - nie kopiujemy referencji tylko tworzymy nowe obiekty powiązane)
+      bez tworzenia zależności pomiędzy kodem programu, a klasami obiektów.
+    */
+
+    @Test
+    public void test() throws CloneNotSupportedException {
+
+        Circle c1 = Circle.builder()
+                .radius(5)
+                .x(1)
+                .y(2)
+                .border(Border.builder()
+                        .color("Red")
+                        .size("2px")
+                        .build())
+                .build();
+
+        Circle deepCopyOfC1 = (Circle) c1.cloning();
+
+        if (c1 == deepCopyOfC1) {
+            System.out.println("==");
+        } else {
+            System.out.println("jestem po deep copy zupełnie nowym obiektem z nowymi referencjami do obiektu klasy Border");
+        }
+
+        //deep copy powoduje uniezależnienie się od referencji do obiektów, które ma orginał (Border) - no bo ma nowe obiekty na podstawie starych wartości.
+         if (c1.getBorder() == deepCopyOfC1.getBorder()) {
+             System.out.println("==");
+         } else {
+             System.out.println("jestem po deep copy zupełnie nowym obiektem z nowymi referencjami do obiektu klasy Border");
+         }
+
+        System.out.println("koniec");
+    }
+}
---
+++ WzorceProjektowe/Prototyp/src/com/company/Triangle.java
@@ -0,0 +1,51 @@
+package com.company;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class Triangle extends Shape {
+
+    @Builder
+    public Triangle(int x, int y, Border border) {
+        super(x, y, border);
+    }
+
+    public static Triangle create(int x, int y, Border border) {
+        return Triangle.builder()
+                .x(x)
+                .y(y)
+                .border(border)
+                .build();
+    }
+
+    @Override
+    public void render() {
+        System.out.println("Render Triangle ...");
+    }
+
+    @Override
+    public Shape cloning() {
+        return (Triangle) this.clone();  //deepCopy
+    }
+
+    @Override
+    //deepCopy
+    public Object clone() {
+        Triangle triangle = null;
+        try {
+            //Note that the super.clone() call returns a shallow copy of an object,
+            // but we set deep copies of mutable fields manually, so the result is correct:
+            triangle = (Triangle) super.clone();
+        } catch (CloneNotSupportedException e) {
+            //only shallow clone
+            triangle = new Triangle(
+                    this.x, this.y, this.getBorder());
+        }
+        //deep clone
+        triangle.border = (Border) this.border.clone();
+        return triangle;
+    }
+}
