bb1c52053fa00888bc8da53cfe1b5445b7c57231
Jacek Sysiak <j4sysiak@gmail.com>
2023-01-21 11:42:01 +0100

    21-01-2023 11:32

--- WzorceProjektowe/Behawioralne/Observer1/src/com/company/Client.java
+++ WzorceProjektowe/Behawioralne/Observer1/src/com/company/Client.java
@@ -13,12 +13,15 @@ public class Client {
         ISubsciber iSubsciber1 = new Subsciber("Jacek Sysiak");
         var iSubsciber2 = new Subsciber("Marek Kowalski");
         var iSubsciber3 = new Subsciber("Jan Nowak");
-        iSubsciber3.update("Andrzej Nowak");
 
         // ISubsciber x = new Subsciber("test");
         // x.update("test");
 
         // publisher to taka maszynka do trzymania informacji o subskrybentacjh i wysyłania powiadomień do nich
+        // Mając Subsciber'a tworzymy klasę Publishera notyfikacji.
+        // Publisher będzię w sobie agregował subscibentów (wrzucał do kolekcji),
+        // czyli posiada listę subscribentów i będzie tą listę modyfikował dodawał / usuwał z listy subscybentów do notyfikacji
+        // oraz wysyłał powiadomienia
         var publisherGazetaWyborcza = new Publisher();
         publisherGazetaWyborcza.subscribe(iSubsciber1);
         publisherGazetaWyborcza.subscribe(iSubsciber2);
--- WzorceProjektowe/Behawioralne/Observer1/src/com/company/Publisher.java
+++ WzorceProjektowe/Behawioralne/Observer1/src/com/company/Publisher.java
@@ -5,10 +5,6 @@ import java.util.List;
 
 public class Publisher {
 
-    // Mając Subsciber'a tworzymy klasę Publishera notyfikacji.
-    // Publisher będzię w sobie agregował subscibentów, czyli posiada listę subscribentów i będzie tą listę modyfikował
-    // dodawał / usuwał z listy subscybentów do notyfikacji
-
     private List<ISubsciber> listOfISubscibers = new ArrayList();
 
     public void subscribe(ISubsciber iSubsciber) {
