8198b26ee5c019f78662db51c4b26a93f2367625
Jacek Sysiak <j4sysiak@gmail.com>
2023-01-21 13:17:17 +0100

    21-01-2023 13:05

--- WzorceProjektowe/Behawioralne/Observer2/src/com/company/Client.java
+++ WzorceProjektowe/Behawioralne/Observer2/src/com/company/Client.java
@@ -7,13 +7,15 @@ public class Client {
     @Test
     public static void test() {
         // uruchamiamy wątek jakiegoś zadania i czekamy kiedy się zakończy wątek z pracy (działania) jakiejś maszynerii Subject
-        new Subject(new Observer1());
+
+        var iObserver1 = new Observer1();
+
+        new Subject(iObserver1);
         new Subject(new Observer2());
         new Subject(new Observer3());
 
-        //hurtowe powiadamianie obserwatorów (czyli klientów) kiedy skończy się wątek z pracy (działania) jakiejś maszynerii Subject
+        // hurtowe powiadamianie obserwatorów (czyli klientów) kiedy skończy się wątek z pracy (działania) jakiejś maszynerii Subject
         var subject2 = new Subject2();
-        subject2.addObserver((IObserver) new Client());
         subject2.addObserver(new Observer1());
         subject2.addObserver(new Observer2());
         subject2.addObserver(new Observer3());
