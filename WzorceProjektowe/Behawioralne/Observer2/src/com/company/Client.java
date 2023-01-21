31f162e1b19d7688473b942ea3437f7f686be548
Jacek Sysiak <j4sysiak@gmail.com>
2023-01-21 13:05:35 +0100

    21-01-2023 13:05

--- WzorceProjektowe/Behawioralne/Observer2/src/com/company/Client.java
+++ WzorceProjektowe/Behawioralne/Observer2/src/com/company/Client.java
@@ -1,11 +1,22 @@
 package com.company;
 
-public class Client implements IObserver {
+import org.testng.annotations.Test;
 
-    //Client wykorzystuje polimorfizm - jest jednocześnie klientem i Observerem
+public class Client {
 
-    @Override
-    public void update() {
-        System.out.println("Zakończono wątek! " + getClass().getName());
+    @Test
+    public static void test() {
+        // uruchamiamy wątek jakiegoś zadania i czekamy kiedy się zakończy wątek z pracy (działania) jakiejś maszynerii Subject
+        new Subject((IObserver) new Client());
+        new Subject(new Client2());
+
+        //hurtowe powiadamianie obserwatorów (czyli klientów) kiedy skończy się wątek z pracy (działania) jakiejś maszynerii Subject
+        var subject2 = new Subject2();
+        subject2.addObserver((IObserver) new Client());
+        subject2.addObserver(new Client2());
+        subject2.addObserver(new Client3());
+        subject2.addObserver(new Client4());
+        subject2.startWork();
     }
+
 }
