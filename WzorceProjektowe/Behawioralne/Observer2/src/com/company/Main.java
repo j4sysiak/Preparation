86915c631f55475214cbf1e43cfb95d796f9cb4a
Jacek Sysiak <j4sysiak@gmail.com>
2023-01-21 18:01:17 +0100

    21-01-2023 17:31

--- WzorceProjektowe/Behawioralne/Observer2/src/com/company/Main.java
+++ WzorceProjektowe/Behawioralne/Observer2/src/com/company/Main.java
@@ -11,13 +11,13 @@ public class Main {
         new Subject(new Client2());
         new Subject(new Client3());
 
-        /*
+
         // hurtowe powiadamianie obserwatorów (czyli klientów) kiedy skończy się wątek z pracy (działania) jakiejś maszynerii Subject
         var subject2 = new Subject2();
         subject2.addObserverToList(new Client1());
         subject2.addObserverToList(new Client2());
         subject2.addObserverToList(new Client3());
         subject2.startWork();
-        */
+
     }
 }
