ccac068e3224bb1dddc6fd68186646ccb94f14d3
Jacek Sysiak <j4sysiak@gmail.com>
2023-09-10 16:50:12 +0200

    sadasdas

--- WzorceProjektowe/Kreacyjne/Builder2/src/com/company/Client.java
+++ WzorceProjektowe/Kreacyjne/Builder2/src/com/company/Client.java
@@ -4,12 +4,13 @@ import org.junit.Test;
 
 public class Client {
 
-
     @Test
     public void test( ) {
         //Using builder to get the object in a single line of code and
         //without any inconsistent state or arguments management issues
-        Computer comp = new Computer.ComputerBuilder().setHDD("500GB")
+        Computer comp = new Computer.ComputerBuilder()
+                .setHDD("500GB")
+                .setRAM("256MB")
                 .build();
         System.out.println(comp);
     }
--- WzorceProjektowe/Kreacyjne/Builder2/src/com/company/Computer.java
+++ WzorceProjektowe/Kreacyjne/Builder2/src/com/company/Computer.java
@@ -1,28 +1,36 @@
 package com.company;
 
-import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
-@Getter
-@ToString
 public class Computer {
 
     private Computer(ComputerBuilder builder) {
-        this.HDD=builder.HDD;
+        this.HDD = builder.HDD;
+        this.RAM = builder.RAM;
     }
 
     //required parameters
     private String HDD;
+    private String RAM;
 
 
-    //Builder Class
+    //Builder Class  (inner class)
+    @ToString
+    @Setter
     public static class ComputerBuilder{
 
         // required parameters
         private String HDD;
+        private String RAM;
 
         public ComputerBuilder setHDD(String hdd) {
             this.HDD = hdd;
+            return this;
+        }
+
+        public ComputerBuilder setRAM(String ram) {
+            this.RAM = ram;
             return this;
         }
 
