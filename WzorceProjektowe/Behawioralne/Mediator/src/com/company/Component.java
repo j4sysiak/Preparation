eefd73c21acbb577397126340be8b4259c2a2d1a
Jacek Sysiak <j4sysiak@gmail.com>
2023-10-03 22:18:21 +0200

    03-10-2023 2217

--- WzorceProjektowe/Behawioralne/Mediator/src/com/company/Component.java
+++ WzorceProjektowe/Behawioralne/Mediator/src/com/company/Component.java
@@ -6,7 +6,8 @@ public abstract class Component {
 
     protected IMediator iMediator;
 
-    // DependencsetIMediator(IMediator iMediator) {
+    // Dependency Injection
+    public void setIMediator(IMediator iMediator) {
         this.iMediator = iMediator;
     }
 }
