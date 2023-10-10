b24172c4b4fe8b1b8d8d84fe1c94c42a6692c3a6
Jacek Sysiak <j4sysiak@gmail.com>
2023-10-10 21:58:03 +0200

    10-10-2023 2132

--- WzorceProjektowe/Behawioralne/TemplateMethod/src/com/company/Generator.java
+++ WzorceProjektowe/Behawioralne/TemplateMethod/src/com/company/Generator.java
@@ -3,12 +3,17 @@ package com.company;
 public abstract class Generator {
 
     // szkielet algorytmu
-    //to jest metoda szablonowa - wykonywana będzie dla każdego z trzech algorytmów (pdf, cvs, excel)w poszczególnych klasach
+    //to jest metoda szablonowa - wykonywana będzie dla każdego z trzech algorytmów (pdf, cvs, excel) w poszczególnych klasach
     public void generateReport() {
         getData();
         preparedData();
         generateFile();
         sendFile();
+    }
+
+    //dla pdf specyficzna metoda wysyłki (@ w pdfGenerator) dla pozostałych taka sama - tzn excel i csv korzysta z tej metody
+    protected void getData() {
+        System.out.println("Base: Get Data ...");
     }
 
     //każda metoda wysyłki ma inny sposób przygotowania danych
@@ -17,10 +22,7 @@ public abstract class Generator {
     //każda metoda wysyłki ma inny sposób generowania pliku
     protected abstract void generateFile();
 
-    //dla pdf specyficzna metoda wysyłki (@ w pdfGenerator) dla pozostałych taka sama - tzn excel i csv korzysta z tej metody
-    protected void getData() {
-        System.out.println("Base: Get Data ...");
-    }
+
 
     //dla wszystkich metod wysyłki taka sama
     protected void sendFile() {
