18db8875029bac271d10c2c22922fb7644bfd27f
Jacek Sysiak <j4sysiak@gmail.com>
2023-10-26 22:55:51 +0200

    25-10-2023 2247

--- WzorceProjektowe/Strukturalne/Adapter/src/com/company/SmsSenderAdapter.java
+++ WzorceProjektowe/Strukturalne/Adapter/src/com/company/SmsSenderAdapter.java
@@ -1,17 +1,18 @@
 package com.company;
 
+import external.library.SmsSender;
+
 public class SmsSenderAdapter implements INotificationSender {
 
-    //tworzymy obiekt (pole w tej klasie) zewnętrznej bibioteki
+    //tworzymy obiekt (pole w tej klasie) zewnętrznej bibioteki: external.library.SmsSender
     //i poprzez ten obiekt dostaniemy się do właściwej metody biblioteki zewnętrznej wysyłającej notyfikacje
-    private SmsSender iSmsSender = new SmsSender();
+    private SmsSender smsSender = new SmsSender();
 
     @Override
     public void sendNotification(int userId, Notification notification) {
 
-        
         // musimy w odpowiedni dla nas sposób zaadoptować zewnętrzny interface SmsSender do naszego INotificationSender
         String userPhoneNr = "1111111111";  //  wybierany z klasy User na podstawie userId
-        iSmsSender.sendSms(userPhoneNr, notification.Title + ":" + notification.Body);
+        smsSender.sendSms(userPhoneNr, notification.Title + ":" + notification.Body);
     }
 }
--- WzorceProjektowe/Strukturalne/Adapter/src/external/library/SmsSender.java
+++ WzorceProjektowe/Strukturalne/Adapter/src/external/library/SmsSender.java
@@ -1,8 +1,9 @@
-package com.company;
+package external.library;
 
 // zewnętrzna klasa do której będziemy budować adapter
 public class SmsSender {
-     void sendSms(String to, String text) {
+
+     public void sendSms(String to, String text) {
         System.out.println("Sending SMS to: " + to + " Body:Text " + text);
     }
 }
