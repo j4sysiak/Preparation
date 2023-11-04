dde9bef5c79fc8a9c7a5aeabfffe93a5bf846b8c
Jacek Sysiak <j4sysiak@gmail.com>
2023-11-04 16:55:51 +0100

    04-11-2023 1655

---
+++ WzorceProjektowe/Kreacyjne/Builder3/src/main/java/co/devfoundry/ClientTest.java
@@ -0,0 +1,15 @@
+package co.devfoundry;
+
+public class ClientTest {
+
+    @org.junit.Test
+    public void test() {
+
+        FlightLeg leg = new FlightLeg
+                .FlightLegBuilder("Las Vegas","Los Angeles")
+                .build();
+
+        System.out.println(leg);
+    }
+
+}
