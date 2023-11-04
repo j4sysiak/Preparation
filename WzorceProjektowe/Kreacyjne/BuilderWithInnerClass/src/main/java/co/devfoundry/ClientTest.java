33dfb9d26e41beb113be80c72f43b9669e24051a
Jacek Sysiak <j4sysiak@gmail.com>
2023-11-04 16:28:38 +0100

    04-11-2023 1620

--- WzorceProjektowe/Kreacyjne/BuilderWithInnerClass/src/main/java/co/devfoundry/ClientTest.java
+++ WzorceProjektowe/Kreacyjne/BuilderWithInnerClass/src/main/java/co/devfoundry/ClientTest.java
@@ -2,9 +2,10 @@ package co.devfoundry;
 
 import co.devfoundry.house.House;
 
-public class Main {
+public class ClientTest {
 
-    public static void main(String[] args) {
+    @org.junit.Test
+    public void test() {
 
 //        House house1 = new House("walls", "floors", "rooms", "windows", "doors", "garage");
 //        House house2 = new House("")
