cb4099e1823e38d3db7451918e0f00a8b19e31b3
Jacek Sysiak <j4sysiak@gmail.com>
2023-11-05 19:06:22 +0100

    05-11-2023 1533

--- WzorceProjektowe/Kreacyjne/BuilderClassicalWayWithDirector3/src/com/company/ClientTest.java
+++ WzorceProjektowe/Kreacyjne/BuilderClassicalWayWithDirector3/src/com/company/ClientTest.java
@@ -10,60 +10,56 @@ public class ClientTest {
 
     @Test
     public void test1() {
-        // bedziemy tworzy MonthlyInvoiceBuilder
-        Director im = Director.builder()
+        // budujemy obiekt Director przy użyciu lombok na bazie klasy MonthlyInvoiceBuilder
+        Director director = Director
+                .builder()
                 .iInvoiceBuilder(new MonthlyInvoiceBuilder())
                 .build();
 
-        // budowniczy
-        im.getIInvoiceBuilder().setVendor("Sanex");
-        im.getIInvoiceBuilder().setNote("dupa-dupa-dupa");
-        var invoice = im.getIInvoiceBuilder().build();  // zwraca obiekt w ostatnim kroku
-        System.out.println(invoice);
+        director.getIInvoiceBuilder().setVendor("Sanex");
+        director.getIInvoiceBuilder().setNote("dupa-dupa-dupa");
+        Invoice invoice = director.getIInvoiceBuilder().build();  // zwraca obiekt w ostatnim kroku
     }
 
     @Test
     public void test2() {
-        // EmptyInvoiceBuilder
-        var iem = Director.builder()
+        // budujemy obiekt Director przy użyciu lombok na bazie klasy EmptyInvoiceBuilder
+        Director director = Director
+                .builder()
                 .iInvoiceBuilder(new EmptyInvoiceBuilder())
                 .build();
 
-        // budowniczy
-        iem.getIInvoiceBuilder().setVendor("Sanex");
-        iem.getIInvoiceBuilder().setNote("dupa-dupa-dupa");
-        var em_invoice = iem.getIInvoiceBuilder().build();  // zwraca obiekt w ostatnim kroku
-        System.out.println(em_invoice);
+        director.getIInvoiceBuilder().setVendor("Sanex");
+        director.getIInvoiceBuilder().setNote("dupa-dupa-dupa");
+        Invoice invoice = director.getIInvoiceBuilder().build();  // zwraca obiekt w ostatnim kroku
     }
 
 
     @Test
     public void test3() {
-        // WithOutVATInvoiceBuilder
-        var im = Director.builder()
+        // budujemy obiekt Director przy użyciu lombok na bazie klasy WithOutVATInvoiceBuilder
+        Director director = Director
+                .builder()
                 .iInvoiceBuilder(new WithOutVATInvoiceBuilder())
                 .build();
 
-        // budowniczy
-        im.getIInvoiceBuilder().setVendor("Sanex");
-        im.getIInvoiceBuilder().setNote("dupa-dupa-dupa");
-        var invoice = im.getIInvoiceBuilder().build();  // zwraca obiekt w ostatnim kroku
-        System.out.println(invoice);
+        director.getIInvoiceBuilder().setVendor("Sanex");
+        director.getIInvoiceBuilder().setNote("dupa-dupa-dupa");
+        Invoice invoice = director.getIInvoiceBuilder().build();  // zwraca obiekt w ostatnim kroku
     }
 
 
 
     @Test
     public void test4() {
-        // MonthlyInvoiceBuilder
-        var im = Director.builder()
+        // budujemy obiekt Director przy użyciu lombok na bazie klasy MonthlyInvoiceBuilder
+        Director director = Director
+                .builder()
                 .iInvoiceBuilder(new MonthlyInvoiceBuilder())
                 .build();
 
-        // budowniczy
-        im.getIInvoiceBuilder().setVendor("Sanex");
-        im.getIInvoiceBuilder().setNote("dupa-dupa-dupa");
-        var invoice = im.getIInvoiceBuilder().build();  // zwraca obiekt w ostatnim kroku
-        System.out.println(invoice);
+        director.getIInvoiceBuilder().setVendor("Sanex");
+        director.getIInvoiceBuilder().setNote("dupa-dupa-dupa");
+        Invoice invoice = director.getIInvoiceBuilder().build();  // zwraca obiekt w ostatnim kroku
     }
 }
