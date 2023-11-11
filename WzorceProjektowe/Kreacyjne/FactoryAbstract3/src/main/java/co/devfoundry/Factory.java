084af00f6d1a2786b123d652719accd18046f336
Jacek Sysiak <j4sysiak@gmail.com>
2023-11-11 17:21:58 +0100

    11-11-2023 1721

---
+++ WzorceProjektowe/Kreacyjne/FactoryAbstract3/src/main/java/co/devfoundry/Factory.java
@@ -0,0 +1,13 @@
+package co.devfoundry;
+
+import co.devfoundry.cars.BMWModel;
+import co.devfoundry.cars.Car;
+import co.devfoundry.cars.FordModel;
+
+public interface Factory {
+
+    Car buildFord(FordModel model);
+
+    Car buildBMW(BMWModel model);
+
+}
