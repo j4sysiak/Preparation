f205a4f606d25334182afe8175a51196f8895019
Jacek Sysiak <j4sysiak@gmail.com>
2023-11-16 21:32:34 +0100

    16-11-2023 2132

--- WzorceProjektowe/Strukturalne/Adapter2/src/main/java/co/devfoundry/ClientTest.java
+++ WzorceProjektowe/Strukturalne/Adapter2/src/main/java/co/devfoundry/ClientTest.java
@@ -8,28 +8,30 @@ public class ClientTest {
     @Test
     public void test() {
 
-        ContinentalSocket continentalSocket = new ContinentalSocket();
-        UKSocket ukSocket = new UKSocket();
-
-        IUKDevice iukRadio = new IUKDevice() {
-            public void powerOn() {
-                System.out.println("London calling to the faraway towns");
+        IContinentalDevice icontinentalRadio = new IContinentalDevice() {
+            public void on() {
+                System.out.println("Gra muzyka");
             }
         };
 
-        IContinentalDevice icontinentalRadio = new IContinentalDevice() {
-            public void on() {
+        IUKDevice iukRadio = new IUKDevice() {
+            public void powerOn() {
                 System.out.println("London calling to the underworld");
             }
         };
 
-     /*   pierwszy sposób: oddzielne  adaptery  dla każdej strony oddzielnie: UKToContinentalAdapter i ContinentalToUKAdapter
-        UKToContinentalAdapter adapter1 = new UKToContinentalAdapter(iukRadio);
-        continentalSocket.plugIn(adapter1);
 
-        ContinentalToUKAdapter adapter2 = new ContinentalToUKAdapter(icontinentalRadio);
-        ukSocket.plugIn(adapter2);
-      */
+
+        // pierwszy sposób: oddzielne  adaptery  dla każdej strony oddzielnie: UKToContinentalAdapter i ContinentalToUKAdapter
+        UKToContinentalAdapter uKToContinentalAdapter = new UKToContinentalAdapter(iukRadio); //  angielski adapter dostaje kontynentalna wtyczke
+        ContinentalSocket continentalSocket = new ContinentalSocket();
+        continentalSocket.plugIn(uKToContinentalAdapter);  // do kontynentalnego gniazdka wpinamy adapter z angielska wtyczka
+
+        ContinentalToUKAdapter continentalToUKAdapter = new ContinentalToUKAdapter(icontinentalRadio);  //  kontynentalny adapter dostaje angielska wtyczke
+        UKSocket ukSocket = new UKSocket();
+        ukSocket.plugIn(continentalToUKAdapter);  // do angielskiego gniazdka wpinamy adapter z kontynentalna wtyczka
+
+
 
         //lub drugi sposób, jeden adapter dla dwóch stron: TwoWayAdapter
          TwoWayAdapter adapter = new TwoWayAdapter(iukRadio, icontinentalRadio);
