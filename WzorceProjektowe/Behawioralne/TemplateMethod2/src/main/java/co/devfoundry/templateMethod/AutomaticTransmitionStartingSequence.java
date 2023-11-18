a4e810cb35239c63bdd616a2c742c54925239bbc
Jacek Sysiak <j4sysiak@gmail.com>
2023-11-18 21:58:48 +0100

    18-11-2023 2158

--- WzorceProjektowe/Behawioralne/TemplateMethod2/src/main/java/co/devfoundry/templateMethod/AutomaticTransmitionStartingSequence.java
+++ WzorceProjektowe/Behawioralne/TemplateMethod2/src/main/java/co/devfoundry/templateMethod/AutomaticTransmitionStartingSequence.java
@@ -3,6 +3,7 @@ package co.devfoundry.templateMethod;
 public class AutomaticTransmitionStartingSequence extends ClassicCarStartingSequence {
 
 
+
     @Override
     public void setTheGear() {
         System.out.println("Automat");
