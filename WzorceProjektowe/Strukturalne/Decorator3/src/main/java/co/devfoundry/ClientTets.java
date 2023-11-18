f4b9d777f0c656eff5718e41a06d631ecf4906ae
Jacek Sysiak <j4sysiak@gmail.com>
2023-11-18 15:03:18 +0100

    18-11-2023 1348

--- WzorceProjektowe/Strukturalne/Decorator3/src/main/java/co/devfoundry/ClientTets.java
+++ WzorceProjektowe/Strukturalne/Decorator3/src/main/java/co/devfoundry/ClientTets.java
@@ -3,18 +3,27 @@ package co.devfoundry;
 public class ClientTets {
 
     public static void main(String[] args) {
-
         generateMap();
     }
 
     private static void generateMap() {
-        //Terrain terrain  = new Plain();
-        //Terrain terrain1 = new Hill();
-        Terrain terrain2 = new SwampDecorator(new Hill());
-        Terrain terrain3 = new SwampDecorator(new ForestDecorator(new Plain()));
 
-        System.out.println("SwampDecorator hill cost "+ terrain2.fuelCost());
-        System.out.println("SwampDecorator forest plain cost " + terrain3.fuelCost());
+        //base terain
+        Hill hill = new Hill();
+        System.out.println("hill " + hill.fuelCost());
+
+
+        //wpuszczamy go w bagna, więc mamsz do pokonania hills (30) w bagnach (15) = 45 zł
+        Terrain hillAndSwamp = new SwampDecorator(hill);
+        System.out.println("hillAndSwamp " + hillAndSwamp.fuelCost());
+
+        //i dodatkowo wpuszczamy go w jeszcze do lasu za (5):  więc suma 45 + 5 = 50 zł
+        Terrain hillAndSwampAndForest = new ForestDecorator(hillAndSwamp);
+        System.out.println("hillAndSwampAndForest " + hillAndSwampAndForest.fuelCost());
+
+        // lub hurtowo w jednym wierszu
+        Terrain terrain = new ForestDecorator(new SwampDecorator(new Hill()));
+        System.out.println("terrain " + terrain.fuelCost());
     }
 
 }
