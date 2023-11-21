c5e735a8dd5177ca023b26f6f2110c9c885bc487
Jacek Sysiak <j4sysiak@gmail.com>
2023-11-21 21:52:17 +0100

    21-11-2023 2152

--- WzorceProjektowe/Behawioralne/State2/src/main/java/co/devfoundry/State.java
+++ WzorceProjektowe/Behawioralne/State2/src/main/java/co/devfoundry/State.java
@@ -3,7 +3,10 @@ package co.devfoundry;
 public interface State {
 
     void insertTheCoin(CoffeeMachine coffeeMachine);
+
     void pushTheButton(CoffeeMachine coffeeMachine);
+
     void takeTheCup(CoffeeMachine coffeeMachine);
+
     void returnTheCoin(CoffeeMachine coffeeMachine);
 }
