8880dac2476c84921bcaebb1e5f0b23cd3d764e7
Jacek Sysiak <j4sysiak@gmail.com>
2023-11-22 22:26:31 +0100

    22-11-2023 2226

---
+++ WzorceProjektowe/Strukturalne/Proxy3/src/main/java/co/devfoundry/Main.java
@@ -0,0 +1,33 @@
+package co.devfoundry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<SavedGame> savedGames = loadSavedGames();
+        listSavedGames(savedGames);
+    }
+
+    public static List<SavedGame> loadSavedGames() {
+
+        int savedGamesCount = 20;
+        List<SavedGame> savedGames = new ArrayList<>(savedGamesCount);
+        for(int i=0;i<20;i++) {
+            SavedGame sg = new SavedGameProxy();
+            sg.initialize();
+            savedGames.add(sg);
+        }
+
+        return savedGames;
+    }
+
+    private static void listSavedGames(List<SavedGame> savedGames) {
+        for(SavedGame savedGame : savedGames) {
+            System.out.println(savedGame.getName());
+        }
+    }
+
+}
---
+++ WzorceProjektowe/Strukturalne/Proxy3/src/main/java/co/devfoundry/SavedGame.java
@@ -0,0 +1,10 @@
+package co.devfoundry;
+
+public interface SavedGame {
+    
+    void initialize();
+
+    void loadGame();
+
+    String getName();
+}
