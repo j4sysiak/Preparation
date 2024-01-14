0fc5f497924a3fe4a2e377f25250ee35c4680b64
Jacek Sysiak <j4sysiak@gmail.com>
2024-01-14 18:07:50 +0100

    14-01-2024 1745

---
+++ FunctionalInterface/src/com/company/DemoFI.java
@@ -0,0 +1,75 @@
+package com.company;
+
+
+@FunctionalInterface
+interface Welcome {
+    void welcomeMessage();
+
+    //moze byc
+    default void methodOne(){
+        System.out.println("Hello One");
+    }
+
+    //moze byc
+    default void methodTwo(){
+        System.out.println("Hello Two");
+    }
+
+    //nie moze byc - może być tylko jedna metoda , więc albo methodOne() alba ta - wybór należy do Ciebie kolego
+    //void absMethod();
+}
+
+
+//Kazdy interface funkcyjny musi mieć:
+//        Some key point -
+//        1. >= It's a Java 8 Feature
+//        2. Exactly ONE Abstract method in an Interface
+//        3. Single Abstract Method Interface (SAM)
+//        4. @FunctionalInterface java annotation is used
+
+
+
+public class DemoFI {
+
+    public static void main(String[] args) {
+
+
+
+        Welcome welcome = () -> System.out.println("Welcome Interface is implemented");
+        welcome.welcomeMessage();
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
