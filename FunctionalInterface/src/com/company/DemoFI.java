de3d605bd4e03fe06b084cff1495b8f608ae36ee
Jacek Sysiak <j4sysiak@gmail.com>
2024-01-14 18:09:02 +0100

    14-01-2024 1745

--- FunctionalInterface/src/com/company/DemoFI.java
+++ FunctionalInterface/src/com/company/DemoFI.java
@@ -33,8 +33,6 @@ public class DemoFI {
 
     public static void main(String[] args) {
 
-
-
         Welcome welcome = () -> System.out.println("Welcome Interface is implemented");
         welcome.welcomeMessage();
 
