505c4897e6e0a7650d28eef79a966a0b8c1f9917
Jacek Sysiak <j4sysiak@gmail.com>
2024-02-04 17:47:20 +0100

    04-02-2024 1746

---
+++ FunctionalInterface/src/com/company/TestFunction.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class TestFunction {
+
+    public static void  main(String [] args) {
+
+        // Function<T, R> is a functional interface i.e. one abstract method:
+        //      R apply(T t)
+        Function<String, Integer> fn2 = s -> s.length();
+        System.out.println("Function: " + fn2.apply("Moscow"));// 6
+
+        // BiFunction<T, U, R> is a functional interface i.e. one abstract method:
+        //      R apply(T t, U u)
+        BiFunction<String, String, Integer> biFn   = (s1, s2) -> s1.length() + s2.length();
+        System.out.println("BiFunction: " + biFn.apply("William", "Shakespeare"));// 18
+
+        BiFunction<String, String, String> biFn2   = (s1, s2) -> s1.concat(s2);
+        System.out.println("BiFunction: " + biFn2.apply("William ", "Shakespeare"));// William Shakespeare
+
+    }
+
+}
---
+++ FunctionalInterface/src/com/company/TestUnaryOperator.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.function.BinaryOperator;
+import java.util.function.UnaryOperator;
+
+public class TestUnaryOperator {
+    public static void  main(String [] args) {
+
+        // UnaryOperator<T> extends Function<T, T> is a functional interface i.e. one abstract method:
+        //      T apply(T t)
+        UnaryOperator<String> unaryOp = name -> "My name is "+name;
+        System.out.println("UnaryOperator: " + unaryOp.apply("Sean"));// My name is Sean
+
+        // BinaryOperator<T> extends BiFunction<T, T, T> is a functional interface i.e. one abstract method:
+        //      T apply(T t1, T t2)
+        BinaryOperator<String> binaryOp   = (s1, s2) -> s1.concat(s2);
+        System.out.println("BinaryOperator: " + binaryOp.apply("William ", "Shakespeare"));// William Shakespeare
+
+    }
+}
+
