90e0b91032726563c721153372469e9cd5027d81
Jacek Sysiak <j4sysiak@gmail.com>
2024-02-06 21:52:39 +0100

    06-02-2024 2028

--- FunctionalInterface/src/com/company/lab/BasicLambdasRart2.java
+++ FunctionalInterface/src/com/company/lab/BasicLambdasRart2.java
@@ -1,6 +1,7 @@
 package com.company.lab;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class BasicLambdasRart2 {
@@ -28,11 +29,90 @@ public class BasicLambdasRart2 {
         */
         basicLambdasRart2.sortAge(listPeople);
 
+        //7
+        /*
+        In main(), invoke the sortName() method passing down listPeople; in sortName() do the following:
+        a) As in 6a except sort the Person objects in ascending name order.
+        b) Output the sorted list using the Iterable forEach() method passing in a lambda expression.
+        */
+        basicLambdasRart2.sortName(listPeople);
+
+        //8
+        /*
+        In main(), invoke the sortHeight() method passing down listPeople; in sortHeight() do the following:
+        a) As in 6a except sort the Person objects in ascending height order.
+        b) Output the sorted list using the Iterable forEach() method passing in a lambda expression.
+        */
+        basicLambdasRart2.sortHeight(listPeople);
 
     }
+
+
+
 
     private void sortAge(List<Person> listPeople) {
+        // sort by age
+        // Note. java.util.List interface supports the sort method directly
+        // we do not need use Collection.sort anymore
+
+        // Comparator<? super E> c
+
+        //1-krok
+        //  listPeople.sort(    potrzebujemy Comparator<? super E> c     )
+        //Comparator comparator = new Comparator() {
+
+        // potrzebujemy jakąś metodę z interfejsem funkcyjnym, który zróci int
+        //np  comparing(...)
+        //public static <T, U extends Comparable<? super U>> Comparator<T> comparing(
+        //        Function<? super T, ? extends U> keyExtractor)
+
+        // Function<Integer, Person> function = new Function() {
+        //     @Override
+        //      public Integer apply(Person person) {
+        //         return person.getAge();
+        //     }
+        // };
+
+        // doprowadzamy do lambda
+        //Function<Person, Integer> function = p -> p.getAge();
+
+        // to nam zwraca Comerator<T> i jesteśmy w domu
+        //Comparator<Integer> comparing(p -> p.getAge())
+
+
+        listPeople.sort(Comparator.comparing(p -> p.getAge()));
+        //lub
+        listPeople.sort(Comparator.comparing(Person::getAge));
+
+
+        // listPeople.forEach(   Consumer<T> c    );
+        // potrzebujemy w parametrze obiektu Consumer<T>
+
+        //Consumer<Person> consumer = new Consumer<Person>() {
+        //    @Override
+        //    public void accept(T t) {
+        //       System.out.println(t.getName());
+        //    }
+
+        // redukujemy do lambda
+        //Consumer<String> consumer = p -> System.out.println(p)
+
+        listPeople.forEach(   p -> System.out.println(p)    );
+        listPeople.forEach(    System.out::println     );
+
     }
+
+    private void sortName(List<Person> listPeople) {
+        listPeople.sort(Comparator.comparing(p -> p.getName()));
+        listPeople.forEach(   p -> System.out.println(p.getName())    );
+    }
+
+    private void sortHeight(List<Person> listPeople) {
+        listPeople.sort(Comparator.comparing(p -> p.getHeight()));
+        listPeople.forEach(   p -> System.out.println(p.getHeight())    );
+    }
+
+
 
     private static List<Person> getPeople() {
         List<Person> result = new ArrayList<>();
--- FunctionalInterface/src/com/company/lab/Person.java
+++ FunctionalInterface/src/com/company/lab/Person.java
@@ -3,14 +3,20 @@ package com.company.lab;
 public class Person {
     private final String name;
     private final Integer age;
+    private final double height;
 
-    public Person(String name, Integer age, double v) {
+    public Person(String name, Integer age, double height) {
         this.name = name;
         this.age = age;
+        this.height = height;
     }
 
     public String getName() {
         return name;
+    }
+
+    public double getHeight() {
+        return height;
     }
 
     public Integer getAge() {
