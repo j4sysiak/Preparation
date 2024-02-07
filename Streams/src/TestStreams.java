81542accc383c233a795752121672b31f82363ec
Jacek Sysiak <j4sysiak@gmail.com>
2024-02-07 20:44:12 +0100

    07-02-2024 1955

--- Streams/src/TestStreams.java
+++ Streams/src/TestStreams.java
@@ -1,23 +1,58 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class TestStreams {
 
     public static void main(String[] args) {
+       // firstPipeline();
+      //  lazyRetrieveDataPipeline();
+        beneficialOfUsingSreamsLazyOPerationPipeline();
+    }
 
+    private static void beneficialOfUsingSreamsLazyOPerationPipeline() {
+        Stream.of("Alex", "David", "April", "Edward")
+                .map(s -> {System.out.println("map: " + s);  // intermediate operation (pośrednia operacja)
+                    return s.toUpperCase();})
+                .anyMatch(s -> {  // ends when first true is returned (Alex)
+                    System.out.println("anyMatch: " + s);
+                    return s.startsWith("A");
+                });
+
+
+        List<String> names = Arrays.asList("April", "Ben", "Charlie", "David", "Benildus", "Christian");
+        names.stream()
+                .peek(System.out::println)
+                .filter(s ->   {
+                           System.out.println("filter1: " + s);
+                           return s.startsWith("B") || s.startsWith("C"); } )
+                .filter(s -> {
+                           System.out.println("filter2: " + s);
+                           return s.length() > 3; } )
+                .limit(1)  // intermediate operation (pośrednia operacja)  Stream<T> limit(long)
+                .forEach(System.out::println);  // terminate operation
+    }
+
+    private static void lazyRetrieveDataPipeline() {
+        Stream.of("Alex", "David", "April", "Edward")
+                .filter(s -> {System.out.println("filter: " + s);
+                              return true;}
+                        )
+                .forEach(s -> System.out.println("forEach: " + s));   // forEach is terminal operation
+    }
+
+    private static void firstPipeline() {
         List<Double> temps = Arrays.asList(99.9, 100.2, 89.88, 102.91);
 
         Long howManyTempsGreaterThen100Degr =
                 temps
                         .stream()
-                       // .peek(System.out::println)
+                        // .peek(System.out::println)
                         .filter(t -> t > 100)
                         .peek(System.out::println)
                         .count();
-
         System.out.println("wynik: " + howManyTempsGreaterThen100Degr);
-
     }
 }
 
