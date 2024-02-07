1f1e2e806b76deb15f7ba4e854a702fb34338cb9
Jacek Sysiak <j4sysiak@gmail.com>
2024-02-07 20:05:11 +0100

    07-02-2024 1955

--- Streams/src/TestStreams.java
+++ Streams/src/TestStreams.java
@@ -1,6 +1,74 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class TestStreams {
 
     public static void main(String[] args) {
-        System.out.println("TestStreams");
+
+        List<Double> temps = Arrays.asList(99.9, 100.2, 89.88, 102.91);
+
+        Long howManyTempsGreaterThen100Degr =
+                temps
+                        .stream()
+                       // .peek(System.out::println)
+                        .filter(t -> t > 100)
+                        .peek(System.out::println)
+                        .count();
+
+        System.out.println("wynik: " + howManyTempsGreaterThen100Degr);
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
