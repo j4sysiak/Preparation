7057e93f0c2e81ce2aacc12f6bf8909e930d4312
Jacek Sysiak <j4sysiak@gmail.com>
2024-02-08 21:39:18 +0100

    08-02-2024 1958

--- Streams/src/TestStreams.java
+++ Streams/src/TestStreams.java
@@ -2,6 +2,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
 import java.util.stream.Stream;
 
 public class TestStreams {
@@ -10,11 +12,116 @@ public class TestStreams {
         // firstPipeline();
         //  lazyRetrieveDataPipeline();
         // beneficialOfUsingSreamsLazyOPerationPipeline();
-
         // collectionStreams();
-        //   mapStreams();
+        //  mapStreams();
         // streamOfExample();
-        streamFromFileLines();
+        // streamFromFileLines();
+        // infiniteStream();
+        // optionalStream();    //Optional<T> it replace null - If stream is empty then Optional will be empty (we do not deal with null at all)
+        // terminalStream();
+        reduceStream();
+    }
+
+    private static void reduceStream() {
+
+    }
+
+    private static void terminalStream() {
+        List<String> names = Arrays.asList("Alan", "Brian", "Colin");
+        Predicate<String> pred = n -> n.startsWith("A");
+        System.out.println(names.stream().allMatch(pred));
+        System.out.println(names.stream().noneMatch(pred));
+
+        // bo
+//        Predicate<String> pred1 = new Predicate<String>() {
+//            @Override
+//            public boolean test(String s) {
+//                return false;
+//            }
+//        };
+//        Predicate<String> pred2 = s -> s.startsWith("A");
+
+        Stream<String> names2 = Stream.of("Cathy", "Pauline", "Zoe");
+        names2.forEach(System.out::println);
+    }
+
+    private static void optionalStream() {
+
+        /*
+        min(Comparator<? super String> comparator)
+        Comparator<? super Integer> comparator1 = new Comparator<Integer>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return 0;
+            }
+        };
+        Comparator<? super String> comparator2 = (a, b) -> a.length() - b.length();
+
+         */
+
+
+
+        Optional<String> min = Stream.of("deer", "horse", "pig", "cat")
+                .min((s1, s2) -> s1.length() - s2.length());  // min(Comparator<? super String> comparator)
+        min.ifPresent(System.out::println);
+
+        // lub
+        Optional<String> min2 = Stream.of("deer", "horse", "pig", "cat")
+                .min(Comparator.comparingInt(String::length));  // min(Comparator<? super String> comparator)
+        min2.ifPresent(System.out::println);
+
+
+
+
+        Optional<Integer> max = Stream.of(4, 6, 2, 12, 9)
+                .max(Comparator.comparingInt(i -> i));
+        max.ifPresent(System.out::println);
+        // lub
+        Optional<Integer> max2 = Stream.of(4, 6, 2, 12, 9)
+                .max(Comparator.comparingInt(i -> i));
+        max2.ifPresent(System.out::println);
+
+
+        Optional<String> any = Stream.of("John", "Paul")
+                .findAny();
+        any.ifPresent(System.out::println);  //John usually :)
+
+        Optional<String> first = Stream.of("John", "Paul")
+                .findFirst();
+        first.ifPresent(System.out::println);  //John
+    }
+
+
+
+    private static void infiniteStream() {
+        // infinite stream of random unordered numbers
+        // between 0 ... 9
+        // Stream<T> generate(Supplier<T> s)
+        // Supplier :    has method  T get();
+
+        Stream<Integer> infStream = Stream.generate(
+                () -> (int) (Math.random() * 10)
+        )
+                .limit(10);
+        //  infinite call  - to stop Ctrl+Z
+         infStream.forEach(System.out::println);
+
+        /*
+        UnaryOperator<Integer> integerUnaryOperator1 = new UnaryOperator<Integer>() {
+            @Override
+            public Integer apply(Integer n) {    //R apply(T t);
+                return n+1;
+            }
+        };
+        //lub lambda
+        UnaryOperator<Integer> integerUnaryOperator = n -> n+1;
+
+         */
+
+        Stream<Integer> infStream2 = Stream.iterate(2, n -> n+1)         //   pierwsze raz(2, 3)   drugi (3, 4)  trzeci (4, 5)  etc
+                .limit(10);
+
+        infStream2.forEach(System.out::println);
     }
 
     private static void streamFromFileLines() {
