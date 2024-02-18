53a096e8ea05c8e692b2476bfcf9251421f6f123
Jacek Sysiak <j4sysiak@gmail.com>
2024-02-18 13:39:58 +0100

    18-02-2024 1111

--- Streams/src/lab/Q2_1840.java
+++ Streams/src/lab/Q2_1840.java
@@ -7,19 +7,31 @@ public class Q2_1840 {
     public static void main(String[] args) {
 
         /*
-        This question demonstrates lazy evaluation. Declare the following List<Integer> ls =
-Arrays.asList(11, 11, 22, 33, 33, 55, 66);
-a. stream the List (note that this is possible because List is a Collection and Collection defines a
-stream() method); ensure only distinct (unique) numbers are streamed; check if “any match”
-11. You should get true for this.
-b. stream the List again (this is necessary because once a stream is closed by a previous terminal
-operation, you must re-create the stream); check to see if “none match” the expression
-x%11>0. Note that the terminal operation noneMatch(Predicate) needs to return false for
-every element in the stream for noneMatch() to return true. In other words, “none of them
-match this….that’s correct, none of them do; return true”. You should get true here as well.
+        This question demonstrates lazy evaluation.
+        Declare the following list :
+         List<Integer> ls = Arrays.asList(11, 11, 22, 33, 33, 55, 66);
+
+            a. stream the List (note that this is possible because List is a Collection and Collection defines astream() method);
+            ensure only distinct (unique) numbers are streamed;
+            check if “any match”. You should get true for this.
+
+            b. stream the List again (this is necessary because once a stream is closed by a previous terminaloperation,
+            you must re-create the stream);
+            check to see if “none match” the expression x%11>0.
+            Note that the terminal operation noneMatch(Predicate) needs to return false for every element
+            in the stream for noneMatch() to return true.
+            In other words, “none of them match this….that’s correct, none of them do; return true”.
+            You should get true here as well.
         */
 
         List<Integer> ls = Arrays.asList(11, 12, 22, 33, 33, 55, 66);
+
+
+
+
+
+
+
         
         // 1. distinct()
 //        System.out.println("distinct");
--- Streams/src/lab/Q2_2024.java
+++ Streams/src/lab/Q2_2024.java
@@ -1,6 +1,11 @@
 package lab;
 
+import java.util.Optional;
 import java.util.OptionalDouble;
+import java.util.function.DoubleConsumer;
+import java.util.function.DoubleFunction;
+import java.util.function.DoublePredicate;
+import java.util.function.ToDoubleFunction;
 import java.util.stream.DoubleStream;
 import java.util.stream.Stream;
 
@@ -11,24 +16,53 @@ public class Q2_2024 {
         There are two parts:
             a. Generate a DoubleStream using the of() method consisting of the numbers 0, 2 and 4.
             Note that this stream is a stream of primitives and not a stream of types.
-            Filter in odd numbers (niepazyste) only and sum the remaining stream. You should get 0. (QID 2.2024)
+            Filter in odd numbers (niepazyste) only and sum the remaining stream.
+            You should get 0. (QID 2.2024)
+
             b. Using 1.0 and 3.0, generate a stream of Double’s.
             Map them to primitive double’s.
-            Filter in even numbers only and calculate the average.
+            Filter in even numbers (parzyste) only and calculate the average.
             Output the result without running the risk of generating an exception.
         */
 
+       // a.
+        double sum = DoubleStream.of(0, 2, 4)
+                .filter(v -> v % 2 != 0)  // public interface DoublePredicate {  // boolean test(double value);
+                .sum()  // 0.0
+        ;
+
+        // b.
+        Stream<Double> doubleStream = Stream.of(1.0, 3.0);
+
+        Stream.of(1.0, 3.0)
+                .mapToDouble(d -> d)  // public interface ToDoubleFunction<T> {  // double applyAsDouble(T value);
+                .filter(v -> v % 2 == 0)   // public interface DoublePredicate {   // boolean test(double value)
+                .average()
+                .ifPresent(v -> System.out.println(v));   // public interface DoubleConsumer {  // void accept(double value);
+
+
+        DoublePredicate predicate = new DoublePredicate() {
+            @Override
+            public boolean test(double v) {
+                return v % 2 == 0;
+            }
+        };
+        DoublePredicate predicate2 = v -> v % 2 == 0;
+
+        DoubleConsumer consumer = new DoubleConsumer() {
+            @Override
+            public void accept(double value) {
+                System.out.println(value);
+            }
+        };
+        DoubleConsumer consumer2 = v -> System.out.println(v);
 
 
 
 
-
-
-
-
-        // 1. sum()
+                // 1. sum()
         DoubleStream is = DoubleStream.of(0, 2, 4); //1 
-        double sum = is.filter( i->i%2 != 0 ).sum(); // sum() is a terminal operation (reduction also) 
+        double sum1 = is.filter( i->i%2 != 0 ).sum(); // sum() is a terminal operation (reduction also)
         System.out.println("sum is "+sum); // 0.0
 
         // 2. average()
