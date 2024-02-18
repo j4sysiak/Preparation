4c8a8c458d033fa51fa742ae04ee824e98ddbfa8
Jacek Sysiak <j4sysiak@gmail.com>
2024-02-18 12:48:00 +0100

    18-02-2024 1111

--- Streams/src/lab/Q2_1858.java
+++ Streams/src/lab/Q2_1858.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Q2_1858 {
     public static void main(String[] args) {
@@ -22,24 +24,63 @@ public class Q2_1858 {
         List<AnotherBook> books = Arrays.asList(  
                 new AnotherBook("Gone with the wind", "Fiction"),        
                 new AnotherBook("Bourne Ultimatum", "Thriller"),         
-                new AnotherBook("The Client", "Thriller") );  
+                new AnotherBook("The Client", "Thriller") );
+
+        List<List<String>> list1 = books.stream()
+                .map(b -> Arrays.asList(b.getGenre()))   // public interface Function<T, R> {  // R apply(T t);
+                .collect(Collectors.toList());
+
         List<String> genreList = new ArrayList<>();
 
+        Function<AnotherBook, List<String>> function = new Function<AnotherBook, List<String>>() {
+            @Override
+            public List<String> apply(AnotherBook books) {
+                genreList.add(books.getGenre());
+                return genreList;
+            }
+        };
+        Function<AnotherBook, List<String>> lamfunction1 = b -> Arrays.asList(b.getGenre());
+        Function<AnotherBook, List<String>> lamfunction2 = b -> genreList;
+
+
+        Stream<List<String>> stream = books.stream()
+                 // .map(book -> book.getGenre())     // lambda
+                .map(b -> genreList)     // lambda
+                ;
+
+        List<String> list2 = books.stream()
+                // .map(book -> book.getGenre())     // lambda
+                //.map(b -> genreList)     // lambda
+                .map(AnotherBook::getGenre)
+                .collect(Collectors.toList());
 
 
 
 
+        //public interface Consumer<T> {  void accept(T t);
+        Consumer<String> consumer = new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+
+            }
+        };
 
 
+        Stream<String> stream1 = books.stream()
+                .map(book -> book.getGenre());// lambda
+//               .map(AnotherBook::getGenre)       // method reference
+//                .map(                           // anonymous inner class
+//                    new Function(){
+//                        @Override
+//                        public String apply(Object o){
+//                            return ((AnotherBook)o).getGenre();
+//                        }
+//                    }
+//                )
+//            .forEach(s->genreList.add(s));    // lambda
 
-
-
-
-
-
-
-
-
+        stream1.forEach(genreList::add);  // void  tylko dodaje elementy do istniejącej już listy Stringów
+        System.out.println(genreList);
 
 
         books.stream()
@@ -66,5 +107,5 @@ public class Q2_1858 {
 //                );
         System.out.println(genreList);
     }
-    
+
 }
