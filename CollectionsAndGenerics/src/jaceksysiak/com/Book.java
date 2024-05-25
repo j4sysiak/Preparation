519654ac2b89da26467c5ee7b1c6c3cfc39f4f99
Jacek Sysiak <j4sysiak@gmail.com>
2024-05-25 22:19:46 +0200

    25-05-2024 2219

---
+++ CollectionsAndGenerics/src/jaceksysiak/com/Book.java
@@ -0,0 +1,27 @@
+package jaceksysiak.com;
+
+public class Book {
+    private String title;
+    private Double price;
+
+    public Book(String title, Double price) {
+        this.title=title;
+        this.price=price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
